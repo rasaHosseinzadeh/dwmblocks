8dc82457e7a561e5b65ffba6e11751bb22956aca
Rasa <rasa.hosseinzadeh@gmail.com>
2021-06-18 16:57:24 -0400

    Add update signals.

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",      "sb-memory",    1,  0},
-    {"",      "sb-cpu",       1,  0},
-    {"",      "sb-disk",      60, 0},
-    {"",      "sb-internet",  10, 0},
-    {"",      "sb-nettraf",   1,  0},
-    {"",      "sb-light",     1,  0},
-    {"",      "sb-volume",    1,  0},
-    {"",      "sb-temp",      10, 0},
-    {"",      "sb-battery",   60, 0},
-    {"",      "sb-clock",     10, 0}, 
+    {"",      "sb-memory",    1,  11},
+    {"",      "sb-cpu",       1,  12},
+    {"",      "sb-disk",      60, 13},
+    {"",      "sb-internet",  10, 14},
+    {"",      "sb-nettraf",   1,  15},
+    {"",      "sb-light",     60, 16},
+    {"",      "sb-volume",    60, 17},
+    {"",      "sb-temp",      10, 18},
+    {"",      "sb-battery",   1 , 19},
+    {"",      "sb-clock",     10, 20}, 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
