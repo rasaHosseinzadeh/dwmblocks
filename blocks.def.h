225fe51de873e08727d6f233e9e794b7220da551
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2021-11-25 21:11:59 -0500

    Decrease update interval.

--- blocks.def.h
+++ blocks.def.h
@@ -2,15 +2,15 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"",      "sb-nettraf",   1,  15},
-    {"",      "sb-internet",  10, 14},
+    {"",      "sb-internet",  1, 14},
     {"",      "sb-memory",    1,  11},
     {"",      "sb-cpu",       1,  12},
     {"",      "sb-disk",      60, 13},
-    {"",      "sb-light",     60, 16},
-    {"",      "sb-volume",    60, 17},
-    {"",      "sb-temp",      10, 18},
+    {"",      "sb-light",     1, 16},
+    {"",      "sb-volume",    1, 17},
+    {"",      "sb-temp",      1, 18},
     {"",      "sb-battery",   1 , 19},
-    {"",      "sb-clock",     10, 20}, 
+    {"",      "sb-clock",     1, 20}, 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
